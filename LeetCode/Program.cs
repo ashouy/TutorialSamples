08583e959a1da1209bff7fe32b19a9581bc7374f
joaopaulo <joaopaulokad@gmail.com>
2026-01-30 23:28:43 -0300

    adjust leet code folder

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,50 +1,5 @@
-﻿/*
- * Nested string array problem
- * print an arrray of elements that can have a nested array of elements
- * ex.
- * 0: 1
- * 1: 2
- * 2: 3
- * 3.0: blue
- * 3.1: yellow
- * 3.2: red
- * 4: test
-*/
-var nestedList = new List<Object>()
-{
-    "1",
-    "2",
-    new List<Object>(){
-        "3.0",
-        "3.1",
-        new List<Object>(){
-            "3.2.1",
-            "3.2.2",
-            "3.2.3",
-        },
-        "3.3",
-    },
-    new List<Object>(){
-        "4.0",
-        "4.1",
-        "4.2",
-        "4.3",
-    },
-    "5",
-};
+﻿// main
+using LeetCode.problems;
 
-printNestedList(nestedList);
-void printNestedList(Object element)
-{
-    if (element is List<Object>)
-    {
-        foreach (var item in (element as List<Object>))
-        {
-            printNestedList(item);
-        }
-    } else
-    {
-        Console.WriteLine(element);
-    }
-}
-
+NestedStringArray nestedStringArray = new NestedStringArray();
+nestedStringArray.printNestedList(NestedStringArray.inputSample);
---
+++ LeetCode/problems/NestedStringArray.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.problems
+{
+    internal class NestedStringArray
+    {
+        /*
+         * Nested string array problem
+         * print an arrray of elements that can have a nested array of elements
+         * ex.
+         * 0: 1
+         * 1: 2
+         * 2: 3
+         * 3.0: blue
+         * 3.1: yellow
+         * 3.2: red
+         * 4: test
+        */
+        public static readonly List<Object>  inputSample = new List<Object>()
+        {
+            "1",
+            "2",
+            new List<Object>(){
+                "3.0",
+                "3.1",
+                new List<Object>(){
+                    "3.2.1",
+                    "3.2.2",
+                    "3.2.3",
+                },
+                "3.3",
+            },
+            new List<Object>(){
+                "4.0",
+                "4.1",
+                "4.2",
+                "4.3",
+            },
+            "5",
+        };
+        public void printNestedList(Object element)
+        {
+            if (element is List<Object>)
+            {
+                foreach (var item in (element as List<Object>))
+                {
+                    printNestedList(item);
+                }
+            }
+            else
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
